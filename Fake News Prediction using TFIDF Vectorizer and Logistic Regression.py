0464b2b67e8be42b2c0b64691ee78aefe155123d
Adarsha Masalkar <97529973+adarsha-masalkar@users.noreply.github.com>
2022-12-14 17:10:46 +0530

    Update Fake News Prediction using TFIDF Vectorizer and Logistic Regression.py

--- Fake News Prediction using TFIDF Vectorizer and Logistic Regression.py
+++ Fake News Prediction using TFIDF Vectorizer and Logistic Regression.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import re
@@ -15,93 +9,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-
-# In[2]:
-
-
 nltk.download('stopwords')
 
-
-# In[3]:
-
-
-print(stopwords.words('English'))
-
-
-# In[4]:
-
+# print(stopwords.words('English'))
 
 df = pd.read_csv('train.csv')
-
-
-# In[5]:
-
-
-df.head()
-
-
-# In[6]:
-
-
-df.info()
-
-
-# In[7]:
-
-
-df.columns
-
-
-# In[8]:
-
-
-df.shape
-
-
-# In[9]:
-
-
 df.isnull().sum()
-
-
-# In[10]:
-
-
 df = df.fillna('')
 
-
-# In[11]:
-
-
-# Merge title and author
-
-
-# In[12]:
-
-
 df['content'] = df['author'] + ' ' + df['title']
-
-
-# In[13]:
-
-
-df.head()
-
-
-# In[14]:
-
 
 # Stemming is the process of reducing a word to its root word.
 # Eg. singer, singing -> sing
 
-
-# In[15]:
-
-
 stemmer = PorterStemmer()
-
-
-# In[16]:
 
 
 def stemming(content):
@@ -110,74 +31,28 @@ def stemming(content):
     return ' '.join(stemmed_content)
 
 
-# In[17]:
-
-
 df['content'] = df['content'].apply(stemming)
-
-
-# In[18]:
-
 
 X = df['content'].values
 Y = df['label'].values
 
-
-# In[19]:
-
-
 vectorizer = TfidfVectorizer()
-
-
-# In[20]:
-
 
 vectorizer.fit(X)
 
-
-# In[21]:
-
-
 X = vectorizer.transform(X)
-
-
-# In[22]:
-
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 2, stratify = Y)
 
-
-# In[23]:
-
-
 model = LogisticRegression()
-
-
-# In[24]:
-
 
 model.fit(x_train, y_train)
 
-
-# In[25]:
-
-
 predict = model.predict(x_train)
-
-
-# In[26]:
-
 
 accuracy_score = accuracy_score(predict, y_train)
 
-
-# In[27]:
-
-
 print('Accuracy score is ', accuracy_score)
-
-
-# In[28]:
 
 
 def predictor(n):
@@ -189,15 +64,9 @@ def predictor(n):
     else:
         print('Fake News')
 
+        
 predictor(0)
 predictor(1)
 predictor(2)
 predictor(3)
 predictor(4)
-
-
-# In[ ]:
-
-
-
-
